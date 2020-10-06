19621238a57ca1d24132f7e62cda409a18fa2cff
pawanoyo <pawan.saini@oyorooms.com>
2020-10-06 13:40:40 +0530

    logged every entry request

--- src/main/java/com/example/demo/api/CustomerResource.java
+++ src/main/java/com/example/demo/api/CustomerResource.java
@@ -2,6 +2,8 @@ package com.example.demo.api;
 
 import com.example.demo.Service.CustomerService;
 import com.example.demo.model.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -19,47 +21,73 @@ public class CustomerResource {
     @Autowired
     private CustomerService customerService;
 
+    Logger logger = LoggerFactory.getLogger(CustomerResource.class);
+
     @PostMapping
     public Customer addCustomer(@Valid  @RequestBody Customer customer){
         customer.setCustomerFirstName(customer.getCustomerFirstName().toLowerCase());
         customer.setCustomerLastName(customer.getCustomerLastName().toLowerCase());
         customer.setCustomerEmail(customer.getCustomerEmail().toLowerCase());
+
+        logger.info("Client has a request for adding new customer: Name - " + customer.getCustomerFirstName() + " " + customer.getCustomerLastName()
+                + ", Email: "+customer.getCustomerEmail());
         return customerService.addCustomer(customer);
     }
 
     @GetMapping
     public List<Customer> getCustomerId(){
+
+        logger.info("Client has requested for customers list");
         return customerService.getCustomers();
     }
 
     @GetMapping(value="/{customerId}")
     public Customer getCustomer(@PathVariable("customerId") int customerId){
+
+        logger.info("Client has requested a customer of id " + customerId);
         return customerService.getCustomer(customerId);
     }
 
     @GetMapping(value = "/search/{by}/{value}")
     public List<Customer> getCustomersByEmail(@PathVariable("by") String by , @PathVariable("value") String value){
 //        System.out.println(by + " " + value);
+
+        logger.info("Client has requested of customer that has " + by + " = " + value);
         return customerService.getAllByValue(by , value);
     }
 
     @GetMapping(value="/pagination/{pageNumber}/{pageSize}")
     public  List<Customer> getCustomersForPage(@PathVariable("pageNumber") int pageNumber , @PathVariable("pageSize") int pageSize){
+
+        logger.info("Client has requested of customer pagination with window size " + pageSize + " and page number " + pageNumber);
         return customerService.getAllCustomerPaginated(pageNumber, pageSize);
     }
 
     @PutMapping(value = "/{customerId}")
     public Customer updateCustomer(@PathVariable("customerId") int customerId,@Valid @RequestBody Customer customer){
+
+        logger.info("Client has a request for updating a customer(Id: " + customerId +") with Name - "+ customer.getCustomerFirstName() + " " + customer.getCustomerLastName()
+                + ", Email: "+customer.getCustomerEmail());
         return customerService.updateCustomer(customerId, customer);
     }
 
     @DeleteMapping(value="/{customerId}")
     public void deleteCustomer(@PathVariable("customerId") int customerId){
+        logger.info("Client has a request for deleting a customer(CustomerId - "+ customerId + ")");
         customerService.deleteCustomer(customerId);
     }
 
     @PatchMapping(value ="/{customerId}")
     public Customer patchCustomer(@PathVariable("customerId") int customerId, @RequestBody Customer customer){
+
+        customer.setCustomerId(customerId);
+        Customer previous = getCustomer(customerId);
+
+        String str = "Client has a request for updating a customer(Id: " + customerId +") with ";
+        str += (customer.getCustomerFirstName().toLowerCase().equals(previous.getCustomerFirstName())) ? "": ", First Name" ;
+        str += (customer.getCustomerLastName().toLowerCase().equals(previous.getCustomerLastName())) ? "": ", Last Name" ;
+        str += (customer.getCustomerEmail().toLowerCase().equals(previous.getCustomerEmail()))? "": ", Email" ;
+        logger.info(str);
         return customerService.patchCustomer(customerId, customer);
     }
 }
--- src/main/java/com/example/demo/interceptor/ServiceInterceptor.java
+++ src/main/java/com/example/demo/interceptor/ServiceInterceptor.java
@@ -1,5 +1,7 @@
 package com.example.demo.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -11,6 +13,7 @@ import java.util.UUID;
 @Component
 public class ServiceInterceptor implements HandlerInterceptor {
 
+    Logger logger = LoggerFactory.getLogger(ServiceInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
 
@@ -22,8 +25,8 @@ public class ServiceInterceptor implements HandlerInterceptor {
                     remoteAddr = request.getRemoteAddr();
                 }
             }
-            //MDC.put("unique-id", UUID.randomUUID().toString());
-            System.out.println("uuid:" + UUID.randomUUID().toString());
+            MDC.put("unique-id", UUID.randomUUID().toString());
+            logger.info("Client(true-client-ip: " + remoteAddr + ") has a request with UUID: ");
         } catch(Exception e){
             MDC.clear();
         }
